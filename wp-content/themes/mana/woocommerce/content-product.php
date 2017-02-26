51d7573a75a8387ade3dfb41722102361de0595c
rodrigodemingo <rodrigodemingo@gmail.com>
2017-02-26 16:42:22 +0400

    Updating Plugins and Database

---
+++ wp-content/themes/mana/woocommerce/content-product.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The template for displaying product content within loops.
+ *
+ * Override this template by copying it to yourtheme/woocommerce/content-product.php
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+global $product, $woocommerce_loop;
+
+// Store loop count we're currently on
+if ( empty( $woocommerce_loop['loop'] ) )
+	$woocommerce_loop['loop'] = 0;
+
+// Store column count for displaying the grid
+if ( empty( $woocommerce_loop['columns'] ) )
+	$woocommerce_loop['columns'] = apply_filters( 'loop_shop_columns', 4 );
+
+global $layout_sidebar;
+if ( isset($layout_sidebar) && $layout_sidebar != 'full'){
+	$woocommerce_loop['columns'] = 3;
+}
+
+// Ensure visibility
+if ( ! $product->is_visible() )
+	return;
+
+// Increase loop count
+$woocommerce_loop['loop']++;
+
+// Extra post classes
+$classes = array();
+if ( 0 == ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] || 1 == $woocommerce_loop['columns'] )
+	$classes[] = '';
+if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] )
+	$classes[] = '';
+
+// check sidebar enabled
+global $layout_sidebar;
+if( $layout_sidebar!='' && $layout_sidebar!='full'){
+	$classes[] = 'col-lg-4 col-md-4 col-sm-6 col-xxs-6 col-xs-12';
+}
+else{
+	$classes[] = 'col-lg-3 col-md-3 col-sm-4 col-xxs-6 col-xs-12';
+}
+
+?>
+<li <?php post_class( $classes ); ?>>
+	<div itemscope itemtype="http://data-vocabulary.org/Product" class="entry_product">
+
+		<?php do_action( 'woocommerce_before_shop_loop_item' ); ?>
+
+		
+
+			<?php
+				/**
+				 * woocommerce_before_shop_loop_item_title hook
+				 *
+				 * @hooked woocommerce_show_product_loop_sale_flash - 10
+				 * @hooked woocommerce_template_loop_product_thumbnail - 10
+				 */
+				do_action( 'woocommerce_before_shop_loop_item_title' );
+			?>
+
+			<h3> <a itemprop="name" href="<?php the_permalink(); ?>"><?php the_title(); ?></a> </h3>
+
+			<?php
+				/**
+				 * woocommerce_after_shop_loop_item_title hook
+				 *
+				 * @hooked woocommerce_template_loop_price - 10
+				 */
+				do_action( 'woocommerce_after_shop_loop_item_title' );
+			?>
+
+		<?php do_action( 'woocommerce_after_shop_loop_item' ); ?>
+	</div>
+</li>
---
+++ wp-content/themes/mana/woocommerce/myaccount/form-login.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Login Form
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+global $woocommerce; ?>
+
+<?php $woocommerce->show_messages(); ?>
+
+<?php do_action('woocommerce_before_customer_login_form'); ?>
+
+<?php if (get_option('woocommerce_enable_myaccount_registration')=='yes') : ?>
+
+<div class="row" id="customer_login">
+
+	<div class="col-lg-6 col-md-6 col-sm-12 col-xxs-6 col-xs-6">
+
+<?php endif; ?>
+
+		<h2 class="woo_title"><?php _e( 'Login', 'themeton' ); ?></h2>
+		<form method="post" class="login">
+			<p class="form-row form-row-first">
+				<label for="username"><?php _e( 'Username or email', 'themeton' ); ?> <span class="required">*</span></label>
+				<input type="text" class="input-text" name="username" id="username" />
+			</p>
+			<p class="form-row form-row-last">
+				<label for="password"><?php _e( 'Password', 'themeton' ); ?> <span class="required">*</span></label>
+				<input class="input-text" type="password" name="password" id="password" />
+			</p>
+			<div class="clear"></div>
+
+			<p class="form-row">
+				<?php $woocommerce->nonce_field('login', 'login') ?>
+				<input type="submit" class="button" name="login" value="<?php _e( 'Login', 'themeton' ); ?>" />
+				<a class="lost_password" href="<?php echo esc_url( wc_lostpassword_url() ); ?>"><?php _e( 'Lost Password?', 'themeton' ); ?></a>
+			</p>
+		</form>
+
+<?php if (get_option('woocommerce_enable_myaccount_registration')=='yes') : ?>
+
+	</div>
+
+	<div class="col-lg-6 col-md-6 col-sm-12 col-xxs-6 col-xs-6">
+
+		<h2 class="woo_title"><?php _e( 'Register', 'themeton' ); ?></h2>
+		<form method="post" class="register">
+
+			<?php if ( get_option( 'woocommerce_registration_email_for_username' ) == 'no' ) : ?>
+
+				<p class="form-row form-row-first">
+					<label for="reg_username"><?php _e( 'Username', 'themeton' ); ?> <span class="required">*</span></label>
+					<input type="text" class="input-text" name="username" id="reg_username" value="<?php if (isset($_POST['username'])) echo esc_attr($_POST['username']); ?>" />
+				</p>
+
+				<p class="form-row form-row-last">
+
+			<?php else : ?>
+
+				<p class="form-row form-row-wide">
+
+			<?php endif; ?>
+
+				<label for="reg_email"><?php _e( 'Email', 'themeton' ); ?> <span class="required">*</span></label>
+				<input type="email" class="input-text" name="email" id="reg_email" value="<?php if (isset($_POST['email'])) echo esc_attr($_POST['email']); ?>" />
+			</p>
+
+			<div class="clear"></div>
+
+			<p class="form-row form-row-first">
+				<label for="reg_password"><?php _e( 'Password', 'themeton' ); ?> <span class="required">*</span></label>
+				<input type="password" class="input-text" name="password" id="reg_password" value="<?php if (isset($_POST['password'])) echo esc_attr($_POST['password']); ?>" />
+			</p>
+			<p class="form-row form-row-last">
+				<label for="reg_password2"><?php _e( 'Re-enter password', 'themeton' ); ?> <span class="required">*</span></label>
+				<input type="password" class="input-text" name="password2" id="reg_password2" value="<?php if (isset($_POST['password2'])) echo esc_attr($_POST['password2']); ?>" />
+			</p>
+			<div class="clear"></div>
+
+			<!-- Spam Trap -->
+			<div style="left:-999em; position:absolute;"><label for="trap">Anti-spam</label><input type="text" name="email_2" id="trap" tabindex="-1" /></div>
+
+			<?php do_action( 'register_form' ); ?>
+
+			<p class="form-row">
+				<?php $woocommerce->nonce_field('register', 'register') ?>
+				<input type="submit" class="button" name="register" value="<?php _e( 'Register', 'themeton' ); ?>" />
+			</p>
+
+		</form>
+
+	</div>
+
+</div>
+<?php endif; ?>
+
+<?php do_action('woocommerce_after_customer_login_form'); ?>
